cbfe2db313c937344f4ed7044061187feb71388e
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-06-13 22:16:57 +0100

    files update

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
 "use strict"
-console.log("yada, yada, yada");
 
-let answer1 = confirm("Are you ready to know me better");
-console.log(answer1);
-
-if(answer1) {
-    console.log("letes get ready for rumble");
-} else {
-    console.log("jab, jab, right hook");
+function userName() {
+    user = prompt("Hi stranger! Please tell me your name.");
+    alert("Welcome to my site, " + user + "!");
+    console.log(user);
 }
 
-let first = false;
-let second = false;
-let third = true;
+userName();
 
-if(first && third) {
-    console.log ("first and third were both true");
-} else if (first || second ) {
-    console.log("first or second was truwe"); 
-} else if (third) {
-    console.log("Third....");
-} else {
-    console.log = prompt("whats your fav color?");
+alert("Hi, I am Yaga. Welcome to my site!");
+
+let myName = prompt  ("Would you like to enter my dreamy world?").toLowerCase();
+
+while (myName != "yes" && myName != "no" && myName !="y" && myName != "n") {
+    alert("Please answer yes/no or y/n only");
+    myName = prompt("Is my name Yaga? Please answer yes/no or y/n only.").toLowerCase();
 }
 
-let color = prompt("what is your fav color?");
+if (myName === "yes" || myName === "y") {
+    //console.log("Correct, my name is Yaga!");
+    alert("Correct, my name is Yaga");
+} else if (myName === "no" || myName === "n") {
+    // console.log("That is not correct!");
+    alert("That is not correct!");
 
-switch (color.toLowerCase()) {
-    case "red":
-        console.log("your fav color is read");
-        break;
-    case "blue":
-        console.log("your fav color is blue");
-        break;
-    default:
-    console.log("No idea what that color is!!!")
 }
-
